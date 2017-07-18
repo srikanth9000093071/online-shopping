48469b0303d99d277aab69e2069a43d467d5ba8a
Srikanth Machavaram <srikanth9000093071@gmail.com>
2017-07-18 11:53:01 +0530

    Fixed ajax issue in admin activating and deactivating the product

--- onlineshopping/src/main/webapp/assets/js/myapp.js
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -24,6 +24,18 @@ $(function(){
 	}
 	
 	
+	// to tackle the csrf token
+	var token = $('meta[name="_csrf"]').attr('content');
+	var header = $('meta[name="_csrf_header"]').attr('content');
+	
+	if(token.length > 0 && header.length > 0) {		
+		// set the token header for the ajax request
+		$(document).ajaxSend(function(e, xhr, options) {			
+			xhr.setRequestHeader(header,token);
+			
+		});				
+	}
+	
 	//Code for JQuery Data-Tables
 	/*// Create a dataset
 	
